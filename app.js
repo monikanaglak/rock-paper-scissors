a8ef72e44669274970620d04d93bb1c78e683824
monika <monikanaglak@outlook.fr>
2023-07-03 17:12:29 +0200

    adding counter

--- app.js
+++ app.js
@@ -3,20 +3,27 @@ let user = document.createElement('div');
 let computer = document.createElement('div');
 let userChoice = document.createElement('p');
 let computerChoiceDisplay = document.createElement('p');
-/*******user informations********************************************************************** */
+let compterComputer = document.createElement('p');
+let compterSays = document.createElement('p')
+/*******user informations***********************************************************************/
 
 user.classList.add('user-box');
-user.textContent = 'User'
-userChoice.textContent ="test";
-
-gameGrid.appendChild(user)
+user.textContent = 'User';
+gameGrid.appendChild(user);
 
 
-/*************************computer box******************************************************** */
-computer.innerHTML = 'computer'
+/*************************computer box*********************************************************/
+computer.innerHTML = 'Computer'
 computer.classList.add('computer')
 computer.textContent = 'Computer';
+
+compterComputer.innerHTML = "counter"+" "+"0";
+computer.appendChild(computerChoiceDisplay);
+computer.appendChild(compterComputer)
+
 gameGrid.appendChild(computer)
+/*************************compteur***********************************************************/
+/************array of choice*******************************************************************/
 const choice = ['paper','rock','scissors'];
 
 function clicking(e){
@@ -25,6 +32,7 @@ function clicking(e){
     user.appendChild(userChoice)
     computerChoosing(choice)
 }
+/**************************making buttons*******************************************************/
 for(let i=0;i<choice.length; i++){
     const button = document.createElement('button');
     button.classList.add('btn')
@@ -34,8 +42,9 @@ for(let i=0;i<choice.length; i++){
     gameGrid.appendChild(button)
 }
 function computerChoosing(choice){
-    computerChoice.textContent = choice[Math.floor(choice.length*Math.random())];
-    computer.appendChild(computerChoice)
-    console.log(computerChoice.textContent)
+    let computerChoice = choice[Math.floor(choice.length*Math.random())];
+    computerChoiceDisplay.innerHTML = computerChoice
+    computer.appendChild(computerChoiceDisplay)
     
 }
+/******************************************getting to know who won****************************** */

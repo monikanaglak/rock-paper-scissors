cd5369252e07ee251a7467c18f7d741e3496944f
monika <monikanaglak@outlook.fr>
2023-07-13 22:19:47 +0200

    making transition

--- js/transition.js
+++ js/transition.js
@@ -1,40 +1,48 @@
-function pageTransition(){
-    console.log("je suis dans quitage de la page")
-    var tl = gsap.timeline();
-    tl.to('ul.transition li',{duration:10,scaleY:1,transformOrigin:"bottom left",stagger:.2})
-    tl.to('ul.transition li',{duration:2,scaleY:0,transformOrigin:"bottom left",stagger:.1})
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.to("ul.transition li", {
+    duration: 0.5,
+    scaleY: 1,
+    transformOrigin: "bottom left",
+    stagger: 0.2,
+  });
+  tl.to("ul.transition li", {
+    duration: 0.5,
+    scaleY: 0,
+    transformOrigin: "bottom left",
+    stagger: 0.1,
+    delay: 0.1,
+  });
 }
-function contentAnimation(){
-    console.log("la je suis entre ")
-    var tl = gsap.timeline();
-    tl.to('ul.transition li',{duration:.5,scaleY:0,transformOrigin:"bottom left",stagger:.2})
-    /* tl.from('.lets',{duration:5,translateX:150,opacity:0})
-    tl.from('.game',{duration:1.5,translateY:150,opacity:0})*/
+function contentAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".lets", { duration: 1.5, tranlateY: 50, opacity: 0 },"-=1.1");
+  tl.to(".lets", { duration: 1.5, tranlateY: 150, opacity: 1 });
 }
 
-const tl =  gsap.timeline()
-function delay(n){
-    n = n || 5000;
-    return new Promise(done=>{
-        setTimeout(()=>{
-            done()
-        },n)
-    })
+const tl = gsap.timeline();
+function delay(n) {
+  n = n || 2000;
+  return new Promise((done) => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
 }
 
 barba.init({
-    sync:true,
-    transitions:[
-        {
-        async leave(){
-            const done = this.async()
-            pageTransition()
-            await delay(1500)
-            done()
-        },
-        async enter(){
-            contentAnimation()
-
-        }
-    }]
-})
+  sync: true,
+  transitions: [
+    {
+      async leave(data) {
+        const done = this.async();
+        pageTransition();
+        await delay(1500);
+        done();
+      },
+      async enter() {
+        contentAnimation();
+      }
+    },
+  ],
+});

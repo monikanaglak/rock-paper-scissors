bdeb20f3cd2d827710b0248ce5d9fb8734c9fd9c
monika <monikanaglak@outlook.fr>
2023-10-25 21:25:57 +0200

    corrections

--- js/app.js
+++ js/app.js
@@ -6,15 +6,14 @@ const computer_choice_display = document.querySelector(
 const resultDisplay = document.querySelector(".result");
 let user_compteur= document.querySelector(".user_compteur");
 let computer_compteur= document.querySelector(".computer_compteur");
-let game_over = document.querySelector(".gameover");
+let game_over = document.querySelector(".welcome_secondp");
 const choice = ["paper", "rock", "scissors"];
 
 all_buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     user_choice_display.textContent = e.target.id;
-    console.log(user_choice_display)
     computer_choice_display.textContent =
-      choice[Math.floor(choice.length * Math.random())];
+    choice[Math.floor(choice.length * Math.random())];
     getResult(user_choice_display, computer_choice_display);
   });
 });
@@ -42,17 +41,25 @@ function getResult(user_choice_display, computer_choice_display) {
 function userwon() {
   resultDisplay.innerHTML = "User Won";
   user_compteur.textContent++;
-  if (user_compteur.innerHTML == 5) {
+  console.log(user_compteur.innerHTML)
+  console.log(computer_compteur.innerHTML)
+  if (user_compteur.innerHTML == 3) {
     game_over.innerHTML = "Game over";
+    restart_game()
   }
 }
 function computerwon() {
   resultDisplay.innerHTML = "Computer Won";
   computer_compteur.textContent++;
-  if (computer_compteur.innerHTML == 5) {
+  if (computer_compteur.innerHTML == 3) {
     game_over.innerHTML = "Game over";
+    restart_game()
   }
 }
 function egalite() {
   resultDisplay.innerHTML = "nobody won";
+}
+function restart_game(){
+  user_compteur.innerHTML = 0;
+  computer_compteur.innerHTML = 0;
 }
